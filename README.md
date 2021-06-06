0c6991068f611b41ebd23c8bf342869667ff94ae
CN80 <cn80@localhost>
2021-06-06 15:47:23 -0400

    update

--- README.md
+++ README.md
@@ -19,13 +19,21 @@ Set up Google Cloud (source https://raw.githubusercontent.com/googleapis/python-
 >3. [Enable the Google Cloud Translate API.](https://cloud.google.com/translate)
 >4. [Setup Authentication.](https://googleapis.dev/python/google-api-core/latest/auth.html)
 
+Set up Oxford dictionaries API:
+
+1. Sign up for one of the [Oxford Dictionary Plans](https://developer.oxforddictionaries.com/?tag=#plans)
+2. Activate account from email
+3. Login to account
+4. Copy credentials from [Applications](https://developer.oxforddictionaries.com/admin/applications) page
+
 Store credentials in `account_key` directory.
 
-When running code, use google cloud account environment variables:
+When running code, setup the environment variables:
 
 ```shell
+export LANGUAGE_LEARN_GOOGLE_PROJECT=projects/project-123
 export GOOGLE_APPLICATION_CREDENTIALS=./account_key/project-123.json
 python main.py
 ```
 
-You can put it into a shell script to make it simpler to launch the program.
+You can put it into a shell script to make it simpler to launch the program. There is an example: [main.sh](./main.sh.example).
--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+import inspect
+import os
+
 import tkinter as tk
 from tkinter import ttk
 
+from google.cloud import translate
 
-class Translation:
-    pass
+
+class TranslationClient:
+    def __init__(self):
+        self.client = translate.TranslationServiceClient()
+        self.project = os.environ.get('LANGUAGE_LEARN_GOOGLE_PROJECT')
+
+    def translate(self, lines, src_lang, dest_langs):
+        results = {}
+        for dest_lang in dest_langs:
+            request = {
+                'parent': self.project,
+                'mime_type': 'text/plain',
+                'source_language_code': src_lang,
+                'target_language_code': dest_lang,
+                'contents': lines,
+            }
+            response = self.client.translate_text(request=request)
+            results[dest_lang] = [
+                translation.translated_text for translation in response.translations
+            ]
+        return results
 
 
 class Application(tk.Frame):
@@ -14,6 +37,8 @@ class Application(tk.Frame):
 
     def __init__(self, master=None):
         super().__init__(master)
+        self.translation_client = TranslationClient()
+        print(self.translation_client.translate(['Hello and welcome to Language Learn', 'This is the second sentence', 'Good day and good afternoon'], 'en', ['fr', 'zh']))
         self.master = master
         self.pack()
         self.create_widgets()
